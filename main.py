c8842730849d8a71d271e1bdbfbe0ead3648c8ba
repl.it user <replituser@example.com>
2022-06-09 12:08:25 +0200

    Change from sqlite to postgres

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "8BYkEfBA6O6donzWlSihBXox7C0sKR6b")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
+# SQLAlchemy database is now pointing to an environment variable that is only avilable on Heroku,
+# so we provide SQLite as the alternative when we're developing the app locally.
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
